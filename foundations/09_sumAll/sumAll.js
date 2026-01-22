3913a158b8a0f2b35a23c3368ec87f7ff94d1b64
Michael Rodriguez <paininquires@gmail.com>
2026-01-21 23:37:18 -0500

    solved 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1 + num2) || num1 < 0 || num2 < 0) {
+        return "ERROR"
+    };
+    if (num1 > num2) {
+        [num1, num2] = [num2, num1];
+    };
+    let all_numbers = [];
+    for (i = num1; i <= num2; i++) {
+        all_numbers.push(i);
+    };
+    return all_numbers.reduce((acc, curr) => acc + curr);
 };
 
 // Do not edit below this line
