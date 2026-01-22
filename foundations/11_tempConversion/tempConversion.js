2932199734a20513d7cf0f8640847648bfe28fd5
Michael Rodriguez <paininquires@gmail.com>
2026-01-22 00:16:58 -0500

    solved 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius_convert = (temp - 32) / 1.8;
+  if (Number.isInteger(celsius_convert)) {
+    return celsius_convert;
+  } else {
+    let rounded_num = celsius_convert.toFixed(1);
+    return Number(rounded_num);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit_convert = (temp * 1.8 + 32);
+  if (Number.isInteger(fahrenheit_convert)) {
+    return fahrenheit_convert;
+  } else {
+    let rounded_num = fahrenheit_convert.toFixed(1);
+    return Number(rounded_num);
+  }
 };
 
 // Do not edit below this line
