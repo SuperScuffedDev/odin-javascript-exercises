60007de10299224ebc872f2b9085925e2d1c7457
Michael Rodriguez <paininquires@gmail.com>
2026-02-05 21:31:35 -0500

    solved 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(arg) {
+    const num = Number(arg);
+    let fib = [0, 1]
+    if (num < 0) {return "OOPS"}
+    for (i=2; i<num+1; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    };
+    return fib[num];
 };
 
 // Do not edit below this line
