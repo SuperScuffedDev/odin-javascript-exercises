404e38aeb9e25c2ba754c6a37081dcfd7a26cde9
Michael Rodriguez <paininquires@gmail.com>
2026-02-05 21:17:39 -0500

    soved 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const lower_str = str
+    .toLowerCase()
+    .split("")
+    .filter((character) => alphanumerical.includes(character))
+    .join("");
+
+    let reverse_str = lower_str.split("").reverse().join("");
+    return are_equal = reverse_str == lower_str
 };
 
 // Do not edit below this line
