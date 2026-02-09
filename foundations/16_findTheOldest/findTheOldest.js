837614057f9bae68cf194ea8f901d553c930e6d2
Michael Rodriguez <paininquires@gmail.com>
2026-02-08 21:26:37 -0500

    solved 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const current_year = new Date().getFullYear();
+    
+    const oldest = array.reduce((acc, curr) => {
+        acc.yearOfDeath ??= current_year;
+        curr.yearOfDeath ??= current_year;
 
+        return ((curr.yearOfDeath - curr.yearOfBirth) > 
+        (acc.yearOfDeath - acc.yearOfBirth)) ? curr : acc
+    });
+
+    return(oldest)
 };
 
 // Do not edit below this line
